2e197ba1c9a02ee5579282b64c62ee11be09d8fe
tamyCabral26 <tamicabralmza20@gmail.com>
2021-09-09 16:08:52 -0300

    Blog

---
+++ BlogCore/BlogCore.Models/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BlogCore.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required (ErrorMessage = "Ingresa un nombre para la categoria")]
+        [Display(Name = "Nombre Categoria")]
+        public string Nombre  { get; set; }
+
+        [Required]
+        [Display(Name = "Orden de visualizacion ")]
+        public string  { get; set; }
+    }
+}
